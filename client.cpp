840402a51c881941cde26dc2ec97aaf58f6def2a
kunkun529 <165275504+kunkun529@users.noreply.github.com>
2024-03-28 19:25:03 +0800

    Add files via upload
    c++ 简易的tcp连接通信

---
+++ client.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+using namespace std;
+
+int main(){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    char ip[] = "127.0.0.1";
+    int port = 1234;
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr)); 
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    {
+        cout << "connect socket error." << endl;
+        return -1;
+    }
+   
+    char buffer[200], msg[200];
+    while(strcmp(msg, "q") != 0){
+        int ret = 0;
+        while(ret != 199){
+            ret = read(sock, buffer, sizeof(buffer)-1);
+            sleep(0.2);
+        }
+        cout << "server: " << buffer << endl;
+        if(strcmp(buffer, "q") == 0){
+            break;
+        }
+        cout << ">>> ";
+        memset(&buffer, 0, sizeof(buffer)); 
+        cin >> msg;
+        write(sock, msg, sizeof(msg));
+    }
+      
+    close(sock);
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+int main(){
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    char ip[] = "127.0.0.1";
+    int port = 1234;
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr)); 
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+    bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    listen(sock, 20);
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    int client_sock = accept(sock, (struct sockaddr*)&client_addr, &client_addr_size);
+
+    char buffer[200], msg[200];
+    while(strcmp(buffer, "q") != 0 ){
+        int ret = 0;
+        cout << ">>> ";
+        cin >> msg;
+        write(client_sock, msg, sizeof(msg));
+        if(strcmp(msg, "q") == 0){
+            break;
+        }
+        while(ret != 199){
+            ret = read(client_sock, buffer, sizeof(buffer)-1);
+            sleep(0.2);
+        }
+        cout << "client: " << buffer << endl;
+    }
+   
+    close(client_sock);
+    close(sock);
+
+    return 0;
+}
+
